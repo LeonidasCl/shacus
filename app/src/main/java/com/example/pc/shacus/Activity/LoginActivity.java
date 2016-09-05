6e9ddec2b667bb28c0a68fcafc558d36a68764b0
licl <476020050@qq.com>
2016-09-05 09:02:19 +0800

    约拍完成

--- app/src/main/java/com/example/pc/shacus/Activity/LoginActivity.java
+++ app/src/main/java/com/example/pc/shacus/Activity/LoginActivity.java
@@ -303,6 +303,7 @@ public class LoginActivity extends AppCompatActivity implements NetworkCallbackI
                 intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(intent);
                 finish();
+                return;
         }
 
 
@@ -325,6 +326,7 @@ public class LoginActivity extends AppCompatActivity implements NetworkCallbackI
                         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                         startActivity(intent);
                         finish();
+                        return;
                     }
 
                     if (eventFlag==3){
@@ -362,7 +364,7 @@ public class LoginActivity extends AppCompatActivity implements NetworkCallbackI
     }
 
     @Override
-    public void exception(IOException e, String requestUrl) {
+    public void exception(IOException e, String requestUrl){
 
     }
 }
